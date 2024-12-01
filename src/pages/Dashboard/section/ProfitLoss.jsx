fb72ab564f48471f7c2ac3295e9cfa19f538fa33
kishormahmud9 <topfazil999@gmail.com>
2024-12-01 18:09:27 +0600

    first commit

--- src/pages/Dashboard/section/ProfitLoss.jsx
+++ src/pages/Dashboard/section/ProfitLoss.jsx
@@ -62,15 +62,15 @@ const ProfitLoss = () => {
           <Grid2 container spacing={4}>
             <Grid2 size={4}>
               <Typography variant="h4">1.74%</Typography>
-              <Paragraph>Jan 2021 -Jun 2021</Paragraph>
+              <Paragraph>Net Profit %</Paragraph>
             </Grid2>
             <Grid2 size={8}>
               <LineChart
                 width={500}
                 height={300}
                 series={[
-                  { data: pData, label: "pv" },
-                  { data: uData, label: "uv" },
+                  { data: pData, label: "Profit" },
+                  { data: uData, label: "Loss" },
                 ]}
                 xAxis={[{ scaleType: "point", data: xLabels }]}
               />
@@ -79,7 +79,10 @@ const ProfitLoss = () => {
         </Grid2>
         <Grid2 size={12}>
           <Grid2 container spacing={4}>
-            <Grid2 size={4}></Grid2>
+            <Grid2 size={4}>
+              <Typography variant="h4">1.74%</Typography>
+              <Paragraph>Net Loss %</Paragraph>
+            </Grid2>
             <Grid2 size={8}></Grid2>
           </Grid2>
         </Grid2>
